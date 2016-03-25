df1b74ddf39c2bb617426d1cac9bc69218c6eacd
Kahilt <kahilt11@gmail.com>
2016-03-25 17:21:48 +0200

    Final upload

--- SpecialCounter.cpp
+++ SpecialCounter.cpp
@@ -1,11 +1,68 @@
 #include "SpecialCounter.h"
+#include <iostream>
 
+using namespace std;
 
-SpecialCounter::SpecialCounter()
+SpecialCounter::SpecialCounter() :beg(0), end(255), inc(1), dec(1)
 {
+	currValue = beg;
 }
 
+
+SpecialCounter::SpecialCounter(int st, int en, int incr, int decr)
+{
+	beg= st;
+	end = en;
+	inc = incr;
+	dec = decr;
+	currValue = beg;
+}
 
 SpecialCounter::~SpecialCounter()
 {
+
 }
+
+void SpecialCounter::start()
+{
+	cout << "Enter number to Begin : ";
+	cin >> beg;				                      //stores user defined value into beg variable
+	currValue = beg;		                     	//sets the Current Value of the counter to the Start value
+}
+
+
+void SpecialCounter::stop()
+{
+	cout << "Enter number to Stop : ";
+	cin >> end;					//stores value from user into end variable
+}
+
+void SpecialCounter::stepInc(int stepSize)	//changes the Increment of the counter
+{
+	inc = stepSize;
+}
+
+void SpecialCounter::stepDec(int stepSize)	//changes the Decrement of the counter
+{
+	dec = stepSize;
+}
+
+void SpecialCounter::operator--()					//counts the counter down by one Decrement
+{
+	currValue = currValue - dec;
+	cout << endl << "The current value is : " << currValue << endl;		//prints the current value
+}
+
+void SpecialCounter::operator++()					//counts the counter Up by one Increment
+{
+	if ((currValue + inc) > end) //ensures that if the counter has reached the stopping value, it will not count up
+	{			
+		cout << "Counter has reached end value." << endl;
+	}
+	else
+	{
+		currValue = currValue + inc;
+		cout << endl << "The current value is : " << currValue << endl;	//prints the current value
+	}
+}
+
--- SpecialCounter.h
+++ SpecialCounter.h
@@ -1,8 +1,21 @@
 #pragma once
 class SpecialCounter
 {
-public:
-	SpecialCounter();
-	~SpecialCounter();
-};
+private:
+	int beg;		//stores starting value
+	int end;		//stores stopping value
+	int inc;		//stores increment value
+	int dec;		//stores decrement value
+	int currValue;	//stores current value of the counter
 
+public:
+	SpecialCounter();		//default constructor
+	~SpecialCounter();      //default destructor
+	SpecialCounter(int, int, int, int);	//default constructor with arguments
+	void start();			//changes Starting value of the counter
+	void stop();			//changes Stopping value of the counter
+	void stepInc(int);		//changes Increment value of the counter
+	void stepDec(int);		//changes Decrement value of the counter
+	void operator++();		//countscounter Up by one Increment
+	void operator--();		//counts counter Down by one Decrement
+};
